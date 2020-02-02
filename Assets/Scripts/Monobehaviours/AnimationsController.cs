e65010ed22b281cf7c22db0cf3e8c990f6e66567
VPavliashvili <chesterapavliashvili@gmail.com>
2020-02-02 17:22:10 +0400

    Animations constructed in animator

---
+++ Assets/Scripts/Monobehaviours/AnimationsController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationsController : MonoBehaviour {
+
+    Animator animator;
+
+    void Awake() {
+        animator = GetComponent<Animator>();    
+    }
+
+    void Start() {
+
+    }
+
+    void Update() {
+
+    }
+
+    public void GetItem() {
+        Debug.Log("Item has been got");
+        //itemPicked = true;
+    }
+
+    public void ThrowItem() {
+        Debug.Log("Item has been thrown");
+    }
+
+    public void EndClimbing() {
+
+    }
+
+    public void ClimbForward() {
+
+    }
+
+    public void ClimbUp() {
+
+    }
+
+}
